be32054b2e30ea2e0b64d21947f349ee7175c9e5
nick <nick.tseng@drsays.org>
2014-10-27 10:07:13 +0800

    ckeditor

--- app/controllers/admin/article_controller.rb
+++ app/controllers/admin/article_controller.rb
@@ -1,6 +1,42 @@
 class Admin::ArticleController < ApplicationController
 
+	# layout 'admin'
 	def index
-		
+
+		@articles = Article.all
+
+	end
+
+
+	def new
+		@article = Article.new
+	end
+
+
+	def create
+		Article.create(article_params)
+		redirect_to root_path
+	end
+
+	def show
+
+		@article = Article.find_by_id(params[:id])
+
+
+
+	end
+
+	def update
+		@article = Article.find_by_id(params[:id])
+		@article.update(article_params)
+
+		redirect_to root_path
+	end
+
+
+	private
+	def article_params
+		params.require(:article).permit(:title,:content)
+
 	end
 end
